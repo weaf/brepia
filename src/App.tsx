ad9a71db7e654b7d874f28fd8aaaaa1f88a2a3b9
weaf <tony.j84@gmail.com>
2026-08-23 09:24:46 +0200

    refactor: mount legacy billing outside auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { AuthProvider } from '@/contexts/AuthProvider';
+import { LegacyBillingProvider } from '@/contexts/LegacyBillingProvider';
 import { TooltipProvider } from './components/ui/tooltip';
 import { Toaster } from './components/ui/toaster';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -29,14 +30,16 @@ function App({ error }: { error?: unknown }) {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <PostHogProvider>
-          <MeshFilesProvider>
-            <TooltipProvider delayDuration={0}>
-              <Toaster />
-              {error !== undefined ? <ErrorView error={error} /> : <Outlet />}
-            </TooltipProvider>
-          </MeshFilesProvider>
-        </PostHogProvider>
+        <LegacyBillingProvider>
+          <PostHogProvider>
+            <MeshFilesProvider>
+              <TooltipProvider delayDuration={0}>
+                <Toaster />
+                {error !== undefined ? <ErrorView error={error} /> : <Outlet />}
+              </TooltipProvider>
+            </MeshFilesProvider>
+          </PostHogProvider>
+        </LegacyBillingProvider>
       </AuthProvider>
     </QueryClientProvider>
   );
