3e208f74386460e97ba9ceaf776dd8c463b00b56
weaf <tony.j84@gmail.com>
2026-08-25 22:03:01 +0200

    feat: keep error toasts until dismissed

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -13,9 +13,22 @@ export function Toaster() {
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {toasts.map(function ({
+        id,
+        title,
+        description,
+        action,
+        variant,
+        duration,
+        ...props
+      }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast
+            key={id}
+            variant={variant}
+            duration={variant === 'destructive' ? 0 : duration}
+            {...props}
+          >
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
