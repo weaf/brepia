f3b26f7c0a60f268815c87d0305a530524114b18
weaf <tony.j84@gmail.com>
2026-08-26 21:06:04 +0200

    feat: export Brepia brand primitives

---
+++ src/components/brand/index.ts
@@ -0,0 +1,4 @@
+export { ActivityIndicator } from './ActivityIndicator';
+export { BrepiaBrand } from './BrepiaBrand';
+export { BrepiaMark } from './BrepiaMark';
+export type { BrepiaMarkTone } from './BrepiaMark';
