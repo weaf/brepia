aec7210e87c57a0d2857bc3403a33eab2f1b92f7
weaf <tony.j84@gmail.com>
2026-09-04 14:42:25 +0200

    Expose explicit AI BRep creation surface

--- src/routes/_layout/_auth/brep/index.tsx
+++ src/routes/_layout/_auth/brep/index.tsx
@@ -1,6 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { BrepAiCreatePanel } from '@/components/brep/BrepAiCreatePanel';
 import { BrepProjectPreview } from '@/components/brep/BrepProjectPreview';
 
 export const Route = createFileRoute('/_layout/_auth/brep/')({
-  component: () => <BrepProjectPreview createProject importPackage />,
+  component: BrepCreatePage,
 });
+
+function BrepCreatePage() {
+  return (
+    <div className="h-full overflow-auto">
+      <BrepAiCreatePanel />
+      <BrepProjectPreview createProject importPackage />
+    </div>
+  );
+}
