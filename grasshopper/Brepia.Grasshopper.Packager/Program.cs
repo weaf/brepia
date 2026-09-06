ba020be5a6f63acc25544f650e495399c0865947
weaf <tony.j84@gmail.com>
2026-09-06 15:34:39 +0200

    Accept Phase 8C package plan transport in packager CLI

--- grasshopper/Brepia.Grasshopper.Packager/Program.cs
+++ grasshopper/Brepia.Grasshopper.Packager/Program.cs
@@ -1,17 +1,23 @@
 using System.Text.Json;
 using Brepia.Grasshopper.Packaging;
 
-if (args.Length != 2)
+var planMode = args.Length == 3 && string.Equals(args[0], "--plan", StringComparison.Ordinal);
+if ((!planMode && args.Length != 2) ||
+    (planMode && args.Length != 3))
 {
     Console.Error.WriteLine(
-        "Usage: Brepia.Grasshopper.Packager <contract.brepia-grasshopper.json> <output.gh>");
+        "Usage: Brepia.Grasshopper.Packager [--plan] <contract-or-plan.json> <output.gh>");
     return 2;
 }
 
 try
 {
-    var contractJson = File.ReadAllText(args[0]);
-    var result = BrepiaGrasshopperDocumentPackager.Write(contractJson, args[1]);
+    var inputPath = args[planMode ? 1 : 0];
+    var outputPath = args[planMode ? 2 : 1];
+    var inputJson = File.ReadAllText(inputPath);
+    var result = planMode
+        ? BrepiaGrasshopperDocumentPackager.WritePackagePlan(inputJson, outputPath)
+        : BrepiaGrasshopperDocumentPackager.Write(inputJson, outputPath);
     Console.WriteLine(JsonSerializer.Serialize(new
     {
         kind = "brepia-grasshopper-package-result",
@@ -19,6 +25,7 @@ try
         result.ProjectId,
         result.SourceRevisionId,
         componentInstanceId = result.ComponentInstanceId,
+        result.GeneratedControlCount,
         result.ByteLength,
         result.OutputPath,
     }));
