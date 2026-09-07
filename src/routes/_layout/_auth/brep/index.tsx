b54b72938ee131edeba721cd115ca0f7bebd9eff
weaf <tony.j84@gmail.com>
2026-09-07 17:36:21 +0200

    Move AI BRep creation to home prompt

--- src/routes/_layout/_auth/brep/index.tsx
+++ src/routes/_layout/_auth/brep/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { BrepAiCreatePanel } from '@/components/brep/BrepAiCreatePanel';
 import { BrepProjectPreview } from '@/components/brep/BrepProjectPreview';
 
 export const Route = createFileRoute('/_layout/_auth/brep/')({
@@ -9,7 +8,6 @@ export const Route = createFileRoute('/_layout/_auth/brep/')({
 function BrepCreatePage() {
   return (
     <div className="h-full overflow-auto">
-      <BrepAiCreatePanel />
       <BrepProjectPreview createProject importPackage />
     </div>
   );
